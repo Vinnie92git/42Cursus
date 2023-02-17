0b2cf9725aae1b690f297e3dfad930b28ed57d9f
Vinnie92git <112777433+Vinnie92git@users.noreply.github.com>
2023-02-17 13:56:12 +0100

    Add files via upload

--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: vipalaci <vipalaci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 13:57:49 by vipalaci          #+#    #+#             */
-/*   Updated: 2023/02/17 11:28:43 by vipalaci         ###   ########.fr       */
+/*   Updated: 2023/02/17 13:55:16 by vipalaci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 # include <stdlib.h> 
 # include <unistd.h>
